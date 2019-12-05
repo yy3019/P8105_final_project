4702814b3a936db2bc2bfd94ad218b8d3fddba4d
NetQuarters <>
2019-12-05 00:13:38 -0500

    edited structure/comments

--- Clinicaltrials_Map/global.R
+++ Clinicaltrials_Map/global.R
@@ -3,8 +3,6 @@ library(readxl)
 
 
 
-
-
 ### function for frames (plotly animation)
 accumulate_by <- function(dat, var) {
   var <- lazyeval::f_eval(var, dat)
@@ -18,54 +16,72 @@ accumulate_by <- function(dat, var) {
 
 
 ## steps for processing data in shiny App
-geocode_state = read_excel("./states.xlsx") #geocode dataset
+# note that default directory for shinyapps.io is within the app folder
+geocode_state <- read_excel("states.xlsx") #geocode dataset
 
-geocode_state =
-  geocode_state %>%
-  separate(location_state,c("location_state","country"),sep = ",") %>%
+## clean the geocode dataframe
+geocode_state <- geocode_state %>%
+  separate(location_state, c("location_state","country"), sep = ",") %>%
   select(-country) %>%
   rename("state_lat" = lat,
-         "state_lng" = lng) # cleaning process
+         "state_lng" = lng)
 
 
-test_us = read_csv("./test_us.csv") %>%
+
+## loading raw data containing clinical trials. Source file is too large; 
+## contact Adobe for raw file
+test_us <- read_csv("./test_us.csv") %>%
   mutate(location_state = recode(location_state,
                                  "Missouri" = "Missouri State")) %>%
-  select(-state_lat,-state_lng) # cleaning process, ready for getting lat/lng of each state
+  select(-state_lat,-state_lng)      # fixing lat/long coded in error from original data
 
-test_us = left_join(test_us, geocode_state) # merge data
+# merge data with the right lat/long
+test_us <- left_join(test_us, geocode_state) 
 
-test_us_completion = test_us %>%
-  mutate(completion_year = str_sub(completion_date,start = -4)) # extract completion years of the studies
+# isolate completion year into dataframe
+test_us_completion <- test_us %>%
+  mutate(completion_year = str_sub(completion_date,start = -4))
 
-completion_by_agency = read_csv('completion_by_agency.csv') # read data in shiny APP
-completion_by_agency$agency_class = factor(completion_by_agency$agency_class) # cleaning process
 
-completion_by_state = read_csv('completion_by_state.csv') # read data in shiny APP
 
-nih_ind = completion_by_agency %>% filter(agency_class == "NIH" | agency_class == "Industry") # studies sponsored only by NIh and Industry
-other_fed = completion_by_agency %>% filter(agency_class != "NIH" & agency_class != "Industry") # studies sponsored only by U.S. Fed and Others
+# reading csv from other code made prior
+completion_by_agency <- read_csv('completion_by_agency.csv') # read data in shiny APP
+completion_by_agency$agency_class <- 
+  factor(completion_by_agency$agency_class)     # turn agency_class to fct
 
-nih_ind = nih_ind %>%
-  accumulate_by(~completion_year) #create frames for animation plot
+completion_by_state <- read_csv('completion_by_state.csv') # read data in shiny APP
+
+# Filter to contain NIH and industry sponsored studies.
+nih_ind <- completion_by_agency %>% 
+  filter(agency_class == "NIH" | agency_class == "Industry") # studies sponsored only by NIh and Industry
+
+# Filter to contain US Federal and "others" sponsored studies.
+other_fed <- completion_by_agency %>% 
+  filter(agency_class != "NIH" & agency_class != "Industry") # studies sponsored only by U.S. Fed and Others
+
+
+
+## Making frames for animation plot
+nih_ind <- nih_ind %>%
+  accumulate_by(~completion_year)  # create frames for animation plot
 
 other_fed = other_fed %>%
-  accumulate_by(~completion_year)  #create frames for animation plot
+  accumulate_by(~completion_year)  # create frames for animation plot
 
 completion_by_state = completion_by_state %>%
-  accumulate_by(~completion_year)  #create frames for animation plot
-####
+  accumulate_by(~completion_year)  # create frames for animation plot
 
-####create datasets for plotting map in shiny APP
-df = test_us
-df1 = test_us %>% filter( agency_class == "U.S. Fed")
-df2 = test_us %>% filter( agency_class == "NIH")
-df3 = test_us %>% filter( agency_class == "Industry")
-df4 = test_us %>% filter( agency_class == "Other")
 
-#sponsor dataset for Shiny APP
-sponsor = 
-  test_us %>%
+
+## Create datasets for plotting map in shiny APP
+df <- test_us                                             # all agency-classes
+df1 <- test_us %>% filter( agency_class == "U.S. Fed")    # only US.Fed
+df2 <- test_us %>% filter( agency_class == "NIH")         # only NIH
+df3 <- test_us %>% filter( agency_class == "Industry")    # only Industry
+df4 <- test_us %>% filter( agency_class == "Other")       # Others
+
+# sponsor dataset for Shiny APP
+sponsor <- test_us %>%
   mutate(sponsors = str_replace(sponsors, "Other", " "),
          sponsors = str_replace(sponsors, "NIH", " "),
          sponsors = str_replace(sponsors, "Industry", " "),
@@ -82,21 +98,21 @@ sponsor =
 
 # Shiny UI settings
 choice_map = c("All","US Fed", "NIH", "Industry", "Other")
-choice_study_info = c("Sponsor Type", "Condition Type", "Intervention Type", "Status", "Enrollment", "Phase (Clinical Trial)", "Duration")
+
+choice_study_info = c("Sponsor Type", "Condition Type", 
+                      "Intervention Type", "Status", "Enrollment", 
+                      "Phase (Clinical Trial)", "Duration")
+
 choice_sponsor = c("sponsor_type", "sponsor", "sponsor_num")
+
 choice_time = c("Studies by Start Year", "Studies by Completion Year")
-choice_phase = c("Phase 1/Phase 2", "Phase 3", "Phase 2", "Phase 1", "Phase 4", "Phase 2/Phase 3", "Early Phase 1")
+
+choice_phase = c("Phase 1/Phase 2", "Phase 3", "Phase 2", "Phase 1", 
+                 "Phase 4", "Phase 2/Phase 3", "Early Phase 1")
+
 agency = c("NIH VS Industry",
            "U.S. Fed VS Others")
+
 all_sponsor = unique(sponsor$sponsors)
-
-
-
-
-
-
-
-
-
 
 
--- Clinicaltrials_Map/server.R
+++ Clinicaltrials_Map/server.R
@@ -8,8 +8,7 @@ library(gganimate)
 
 
 
-# font size
-
+# font size and other settings
 t <- list(
   family = "sans serif",
   size = 8)
@@ -26,75 +25,154 @@ t <- list(
 
 shinyServer(function(input, output){
   
-    
   ####
   output$table <- DT::renderDataTable({
     data.table::data.table(sponsor) #%>% 
     #formatStyle(input$selected, background="skyblue", fontWeight='bold')
   })
+  
+  
   ### MAPS ###
   output$map <- renderLeaflet({
     
-    if (input$choice == "All") {
+    if (input$choice == "All") {                                                           # filter for "all" trials
       leaflet() %>% 
         addTiles() %>%
-        addMarkers(data = df %>% filter(study_type == "Interventional"), lng = ~ state_lng, lat = ~ state_lat, group = "Interventional" , clusterOptions = markerClusterOptions()) %>%
-        addMarkers(data = df %>% filter(study_type == "Observational"), lng = ~ state_lng, lat = ~ state_lat, group = "Observational" , clusterOptions = markerClusterOptions()) %>%
-        addMarkers(data = df %>% filter(study_type == "Observational [Patient Registry]"), lng = ~ state_lng, lat = ~ state_lat, group = "Observational [Patient Registry]", clusterOptions = markerClusterOptions()) %>%
-        addMarkers(data = df %>% filter(study_type == "Expanded Access"), lng = ~ state_lng, lat = ~ state_lat, group = "Expanded Access", clusterOptions = markerClusterOptions()) %>%
+        addMarkers(data = df %>% filter(study_type == "Interventional"),                   # filter for study_type
+                   lng = ~ state_lng,                                                      # assign longitude
+                   lat = ~ state_lat,                                                      # assign latitude
+                   group = "Interventional" ,                                              # assign group
+                   clusterOptions = markerClusterOptions()) %>%
+        addMarkers(data = df %>% filter(study_type == "Observational"),                    # repeat steps above for different study_type
+                   lng = ~ state_lng, 
+                   lat = ~ state_lat, 
+                   group = "Observational" , 
+                   clusterOptions = markerClusterOptions()) %>%
+        addMarkers(data = df %>% filter(study_type == "Observational [Patient Registry]"), 
+                   lng = ~ state_lng, 
+                   lat = ~ state_lat, 
+                   group = "Observational [Patient Registry]", 
+                   clusterOptions = markerClusterOptions()) %>%
+        addMarkers(data = df %>% filter(study_type == "Expanded Access"), 
+                   lng = ~ state_lng, 
+                   lat = ~ state_lat, 
+                   group = "Expanded Access", 
+                   clusterOptions = markerClusterOptions()) %>%
         addLayersControl(
           baseGroups = c("Interventional", "Observational","Observational [Patient Registry]","Expanded Access" ),
           options = layersControlOptions(collapsed = FALSE),
           position = c("topleft") 
         ) 
-    } 
-    
-    else if (input$choice == "US Fed") {
+    } else if (input$choice == "US Fed") {                                                   # filter for "US Fed" trials
       leaflet() %>%
         addTiles() %>%
-        addMarkers(data = df1 %>% filter(study_type == "Interventional"), lng = ~ state_lng, lat = ~ state_lat, group = "Interventional" , clusterOptions = markerClusterOptions()) %>%
-        addMarkers(data = df1 %>% filter(study_type == "Observational"), lng = ~ state_lng, lat = ~ state_lat, group = "Observational" , clusterOptions = markerClusterOptions()) %>%
-        addMarkers(data = df1 %>% filter(study_type == "Observational [Patient Registry]"), lng = ~ state_lng, lat = ~ state_lat, group = "Observational [Patient Registry]", clusterOptions = markerClusterOptions()) %>%
-        addMarkers(data = df1 %>% filter(study_type == "Expanded Access"), lng = ~ state_lng, lat = ~ state_lat, group = "Expanded Access", clusterOptions = markerClusterOptions()) %>%
+        addMarkers(data = df1 %>% filter(study_type == "Interventional"), 
+                   lng = ~ state_lng, 
+                   lat = ~ state_lat, 
+                   group = "Interventional", 
+                   clusterOptions = markerClusterOptions()) %>%
+        addMarkers(data = df1 %>% filter(study_type == "Observational"), 
+                   lng = ~ state_lng, 
+                   lat = ~ state_lat, 
+                   group = "Observational" , 
+                   clusterOptions = markerClusterOptions()) %>%
+        addMarkers(data = df1 %>% filter(study_type == "Observational [Patient Registry]"), 
+                   lng = ~ state_lng, 
+                   lat = ~ state_lat, 
+                   group = "Observational [Patient Registry]", 
+                   clusterOptions = markerClusterOptions()) %>%
+        addMarkers(data = df1 %>% filter(study_type == "Expanded Access"), 
+                   lng = ~ state_lng, 
+                   lat = ~ state_lat, 
+                   group = "Expanded Access", 
+                   clusterOptions = markerClusterOptions()) %>%
         addLayersControl(
           baseGroups = c("Interventional", "Observational","Observational [Patient Registry]","Expanded Access" ),
           options = layersControlOptions(collapsed = FALSE),
           position = c("topleft") 
         )        
       
-    } else if (input$choice == "NIH") {
+    } else if (input$choice == "NIH") {                                                       # filter for "NIH" trials
       leaflet() %>%
         addTiles() %>%
-        addMarkers(data = df2 %>% filter(study_type == "Interventional"), lng = ~ state_lng, lat = ~ state_lat, group = "Interventional" , clusterOptions = markerClusterOptions()) %>%
-        addMarkers(data = df2 %>% filter(study_type == "Observational"), lng = ~ state_lng, lat = ~ state_lat, group = "Observational" , clusterOptions = markerClusterOptions()) %>%
-        addMarkers(data = df2 %>% filter(study_type == "Observational [Patient Registry]"), lng = ~ state_lng, lat = ~ state_lat, group = "Observational [Patient Registry]", clusterOptions = markerClusterOptions()) %>%
-        addMarkers(data = df2 %>% filter(study_type == "Expanded Access"), lng = ~ state_lng, lat = ~ state_lat, group = "Expanded Access", clusterOptions = markerClusterOptions()) %>%
+        addMarkers(data = df2 %>% filter(study_type == "Interventional"), 
+                   lng = ~ state_lng, 
+                   lat = ~ state_lat, 
+                   group = "Interventional" , 
+                   clusterOptions = markerClusterOptions()) %>%
+        addMarkers(data = df2 %>% filter(study_type == "Observational"), 
+                   lng = ~ state_lng, 
+                   lat = ~ state_lat, 
+                   group = "Observational" , 
+                   clusterOptions = markerClusterOptions()) %>%
+        addMarkers(data = df2 %>% filter(study_type == "Observational [Patient Registry]"), 
+                   lng = ~ state_lng, 
+                   lat = ~ state_lat, 
+                   group = "Observational [Patient Registry]", 
+                   clusterOptions = markerClusterOptions()) %>%
+        addMarkers(data = df2 %>% filter(study_type == "Expanded Access"), 
+                   lng = ~ state_lng, 
+                   lat = ~ state_lat, 
+                   group = "Expanded Access", 
+                   clusterOptions = markerClusterOptions()) %>%
         addLayersControl(
           baseGroups = c("Interventional", "Observational","Observational [Patient Registry]","Expanded Access" ),
           options = layersControlOptions(collapsed = FALSE),
           position = c("topleft") 
         )
       
-    } else if (input$choice == "Industry") {
+    } else if (input$choice == "Industry") {                                                   # filter for "industry" trials
       leaflet() %>%
         addTiles() %>%
-        addMarkers(data = df3 %>% filter(study_type == "Interventional"), lng = ~ state_lng, lat = ~ state_lat, group = "Interventional" , clusterOptions = markerClusterOptions()) %>%
-        addMarkers(data = df3 %>% filter(study_type == "Observational"), lng = ~ state_lng, lat = ~ state_lat, group = "Observational" , clusterOptions = markerClusterOptions()) %>%
-        addMarkers(data = df3 %>% filter(study_type == "Observational [Patient Registry]"), lng = ~ state_lng, lat = ~ state_lat, group = "Observational [Patient Registry]", clusterOptions = markerClusterOptions()) %>%
-        addMarkers(data = df3 %>% filter(study_type == "Expanded Access"), lng = ~ state_lng, lat = ~ state_lat, group = "Expanded Access", clusterOptions = markerClusterOptions()) %>%
+        addMarkers(data = df3 %>% filter(study_type == "Interventional"), 
+                   lng = ~ state_lng, 
+                   lat = ~ state_lat, 
+                   group = "Interventional" , 
+                   clusterOptions = markerClusterOptions()) %>%
+        addMarkers(data = df3 %>% filter(study_type == "Observational"), 
+                   lng = ~ state_lng, 
+                   lat = ~ state_lat, 
+                   group = "Observational" , 
+                   clusterOptions = markerClusterOptions()) %>%
+        addMarkers(data = df3 %>% filter(study_type == "Observational [Patient Registry]"), 
+                   lng = ~ state_lng, 
+                   lat = ~ state_lat, 
+                   group = "Observational [Patient Registry]", 
+                   clusterOptions = markerClusterOptions()) %>%
+        addMarkers(data = df3 %>% filter(study_type == "Expanded Access"), 
+                   lng = ~ state_lng, 
+                   lat = ~ state_lat, 
+                   group = "Expanded Access", 
+                   clusterOptions = markerClusterOptions()) %>%
         addLayersControl(
           baseGroups = c("Interventional", "Observational","Observational [Patient Registry]","Expanded Access" ),
           options = layersControlOptions(collapsed = FALSE),
           position = c("topleft") 
         )        
       
-    } else {
+    } else {                                                                                    # filter for "other" trials
       leaflet() %>%
         addTiles() %>%
-        addMarkers(data = df4 %>% filter(study_type == "Interventional"), lng = ~ state_lng, lat = ~ state_lat, group = "Interventional" , clusterOptions = markerClusterOptions()) %>%
-        addMarkers(data = df4 %>% filter(study_type == "Observational"), lng = ~ state_lng, lat = ~ state_lat, group = "Observational" , clusterOptions = markerClusterOptions()) %>%
-        addMarkers(data = df4 %>% filter(study_type == "Observational [Patient Registry]"), lng = ~ state_lng, lat = ~ state_lat, group = "Observational [Patient Registry]", clusterOptions = markerClusterOptions()) %>%
-        addMarkers(data = df4 %>% filter(study_type == "Expanded Access"), lng = ~ state_lng, lat = ~ state_lat, group = "Expanded Access", clusterOptions = markerClusterOptions()) %>%
+        addMarkers(data = df4 %>% filter(study_type == "Interventional"), 
+                   lng = ~ state_lng, 
+                   lat = ~ state_lat, 
+                   group = "Interventional" , 
+                   clusterOptions = markerClusterOptions()) %>%
+        addMarkers(data = df4 %>% filter(study_type == "Observational"), 
+                   lng = ~ state_lng, 
+                   lat = ~ state_lat, 
+                   group = "Observational" , 
+                   clusterOptions = markerClusterOptions()) %>%
+        addMarkers(data = df4 %>% filter(study_type == "Observational [Patient Registry]"), 
+                   lng = ~ state_lng, 
+                   lat = ~ state_lat, 
+                   group = "Observational [Patient Registry]", 
+                   clusterOptions = markerClusterOptions()) %>%
+        addMarkers(data = df4 %>% filter(study_type == "Expanded Access"), 
+                   lng = ~ state_lng, 
+                   lat = ~ state_lat, 
+                   group = "Expanded Access", 
+                   clusterOptions = markerClusterOptions()) %>%
         addLayersControl(
           baseGroups = c("Interventional", "Observational","Observational [Patient Registry]","Expanded Access" ),
           options = layersControlOptions(collapsed = FALSE),
@@ -103,10 +181,13 @@ shinyServer(function(input, output){
     }
     
   })
-  # plots
+  
+  
+  
+  # Bar graph that shows total number of trials in the US based on filter input. 
   output$map_plot <- renderPlot({
     
-    if (input$choice == "All"){
+    if (input$choice == "All") {                                     # all sponsor
  
     df %>%
       group_by(location_state) %>%
@@ -122,9 +203,7 @@ shinyServer(function(input, output){
            y = "States",
            title = "Numbers of trials in States (All)")
         
-    }
-    
-    else if(input$choice == "NIH"){
+    } else if (input$choice == "NIH") {                              # NIH sponsor
 
     df2 %>%
       group_by(location_state) %>%
@@ -139,9 +218,8 @@ shinyServer(function(input, output){
         labs(x = "Total",
              y = "States",
              title = "Numbers of trials in States (NIH)")
-    }
-    
-    else if(input$choice == "Industry"){
+      
+    } else if (input$choice == "Industry") {                         # industry sponsor
   
     df3 %>%
       group_by(location_state) %>%
@@ -156,9 +234,8 @@ shinyServer(function(input, output){
         labs(x = "Total",
              y = "States",
              title = "Numbers of trials in States (Industry)")
-    }
-    
-    else if(input$choice == "Other"){
+      
+    } else if (input$choice == "Other") {                            # other sponsor
 
     df4 %>%
       group_by(location_state) %>%
@@ -173,9 +250,8 @@ shinyServer(function(input, output){
         labs(x = "Total",
              y = "States",
              title = "Numbers of trials in States (Others)")
-    }
-    
-    else{
+      
+    } else {                                                        # US Fed sponsor
       
       df1 %>%
         group_by(location_state) %>%
@@ -193,9 +269,12 @@ shinyServer(function(input, output){
       
     } 
     })
-  #plots
+  
+  
+  # line graph showing the fluctuations in completed trials 
   output$map_plotly <- renderPlotly({
-    if(input$agency =="NIH VS Industry"){
+    
+    if (input$agency == "NIH VS Industry") {
       nih_ind %>%
         plot_ly(
           x = ~ completion_year,
@@ -209,7 +288,7 @@ shinyServer(function(input, output){
                xaxis = list(title = "Completion Year"),
                title = "Number of Completed Studies Sponsored by NIH and Industry",
                font = t) %>%
-        add_markers(colors = viridis_pal(option = "D")(2) , frame = ~frame, ids = ~agency_class) %>%
+        #add_markers(colors = viridis_pal(option = "D")(2) , frame = ~frame, ids = ~agency_class) %>%
         animation_opts(150, easing = "elastic", transition = 0, redraw = FALSE) %>%
         animation_button(
           x = 1, xanchor = "right", y = 0, yanchor = "bottom") %>%
@@ -218,7 +297,7 @@ shinyServer(function(input, output){
           hide = T)
     }
     
-    else{
+    else {
       other_fed %>%
         plot_ly(
           x = ~ completion_year,
